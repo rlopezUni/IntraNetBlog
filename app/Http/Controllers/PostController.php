0523f5c319535cdc5772ae1249453bec85026942
nacho <nacho739@hotmail.com>
2022-07-14 09:58:17 -0500

    Se regresa la intra

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,13 +19,13 @@ class PostController extends Controller
     public function index()
     {
 
-        /*
+        
         $posts = Post::where('status',2)->latest('id')->paginate(8);
 
         return view('posts.index',compact('posts'));
-        */
+        
 
-        return view("nps.index");
+       // return view("nps.index");
     }
 
     /**
